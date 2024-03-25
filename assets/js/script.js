eef1a0a716ea28890fd58ef7284ec0577c010971
shwetakadam5 <159270090+shwetakadam5@users.noreply.github.com>
2024-03-25 14:42:11 +1100

    Initial draft version to get the response from api call

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,8 @@ function handleSearchFormSubmit(event) {
     // https://api.openweathermap.org/data/2.5/weather?q=Melbourne,AU&appid=6afdca3269d40e485ee98de1af3ed1db
     // http://api.openweathermap.org/data/2.5/forecast?lat=-37.8547&lon=145.1853&appid=6afdca3269d40e485ee98de1af3ed1db
 
+    //api.openweathermap.org/data/2.5/forecast/daily?lat=-37.8547&lon=145.1853&cnt=5&appid=6afdca3269d40e485ee98de1af3ed1db
+
   //  Read user input from the form
   const cityName = cityNameInputEl.val().trim();
   console.log(cityName);
@@ -50,10 +52,54 @@ fetch(requestUrl)
       console.log(data.main.temp);
       console.log(data.wind.speed);
       console.log(data.main.humidity);
+
+      fetchDailyForecast(data.coord);
+
+
     });
 
 }
 
+
+function fetchDailyForecast(locationCoords) {
+
+    //  Read user input from the form
+  const locationLongitude = locationCoords.lon;
+  const locationLatitude = locationCoords.lat;
+  console.log(locationLongitude);
+  console.log(locationLatitude);
+
+  const params = new URLSearchParams();
+  params.append(`lat`, `${locationLatitude}`);
+  params.append(`lon`, `${locationLongitude}`);
+  params.append(`units`, `imperial`);
+
+//metric Temp is celcius and speed is meter per second
+//imperial Temp is Fahrenheit and speed is miles per hour MPH
+
+  params.append(`appid`, `${API_KEY}`);
+//   ?lat=-37.8547&lon=145.1853&appid=6afdca3269d40e485ee98de1af3ed1db
+
+
+
+const dailyRequestUrl = new URL('http://api.openweathermap.org/data/2.5/forecast');
+dailyRequestUrl.search = params.toString();
+
+
+console.log(dailyRequestUrl);
+
+fetch(dailyRequestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+     
+
+
+    });
+
+}
 
 
 //  Add event listener to the form element, listen for a submit event, and call the `handleSearchFormSubmit` function.
